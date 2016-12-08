b8ae413e719a340eaecdadc2c8cfd408f4d296f8
Behzad Azampour <behy1368@gmail.com>
2016-12-08 20:26:23 +0000

    implemented create user and index users functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,23 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
 
+    if @user.save
+      redirect_to [:products], notice: 'Category created!'
+    else
+      render :new
+    end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :email,
+      :password
+    )
+  end
+
+
 end
